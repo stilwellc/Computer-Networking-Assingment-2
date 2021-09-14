9bdfd889cc0aedb770de82acdf06bc7e2660168b
Collin Stilwell <cns9383@nyu.edu>
2021-09-14 15:18:00 -0400

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,62 @@
+# import socket module
+from socket import *
+# In order to terminate the program
+import sys
+
+
+def webServer(port= 13331):
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    # Prepare a server socket
+    serverSocket.bind(("", port))
+    # Fill in start
+
+    # listen to one connection at a time
+    serverSocket.listen(1)
+
+    # Fill in end
+
+    while True:
+        # Establish the connection
+        # print('Ready to serve...')
+        connectionSocket, addr =  serverSocket.accept()
+        try:
+
+            try:
+                message =  connectionSocket.recv(1024)
+                filename = message.split()[1]
+                f = open(filename[1:])
+                outputdata =  f.read()
+
+                # Send one HTTP header line into socket.
+                # Fill in start
+                connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
+                # print('200 OK')
+                # Fill in end
+
+                # Send the content of the requested file to the client
+                for i in range(0, len(outputdata)):
+                    connectionSocket.send(outputdata[i].encode())
+
+                connectionSocket.send("\r\n".encode())
+                connectionSocket.close()
+            except IOError:
+            # Send response message for file not found (404)
+            # Fill in start
+                connectionSocket.send('HTTP/1.1 404 Not Found\r\n\r\n'.encode())
+                # print('400 Not Found')
+            # Fill in end
+
+            # Close client socket
+            # Fill in start
+                connectionSocket.close()
+            # Fill in end
+
+        except (ConnectionResetError, BrokenPipeError):
+            pass
+
+    serverSocket.close()
+    sys.exit()  # Terminate the program after sending the corresponding data
+
+
+if __name__ == "__main__":
+    webServer(13331)
